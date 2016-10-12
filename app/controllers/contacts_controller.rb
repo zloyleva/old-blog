4fb3f823bc038c6b819ea308d406623be7628fe0
zloyleva <zloyleva@gmail.com>
2016-10-13 00:53:05 +0300

    Save contact on submit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,6 +4,14 @@ def new
 end
 
 def create
+  @contact = Contact.new(contact_params)
+  @contact.save
+end
+
+private
+
+def contact_params
+  params.require(:contact).permit(:email, :message)
 end
 
 end

74350a7b5cd1790223f258b6e50a52708687457c
zloyleva <zloyleva@gmail.com>
2016-10-16 15:48:19 +0300

    Add terms and about peges

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
 
+  get 'terms' => 'pages#terms'
+  get 'about' => 'pages#about'
+
   resources :articles
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

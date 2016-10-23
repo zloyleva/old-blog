6369eb05958efb302fec08418752a1c82329c3a5
zloyleva <zloyleva@gmail.com>
2016-10-23 22:17:43 +0300

    Fix bug on permit username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
 
    before_filter :configure_permitted_parameters, if: :devise_controller?
 
-    protected
+    private
 
         def configure_permitted_parameters
-            devise_parameter_sanitizer.for(:sign_up) << :username
+            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :username) }
         end
 
 end

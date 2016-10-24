b8b8fc715916767df0c0039af4cc12ea610d57b6
zloyleva <zloyleva@gmail.com>
2016-10-24 12:56:39 +0300

    Auto create for author comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,12 +3,13 @@ class CommentsController < ApplicationController
 
   def create
     @article = Article.find(params[:article_id])
-    @article.comments.create(comment_params)
-
+    @comment  =  @article.comments.new(author: current_user.username, body:  comment_params[:body])
+    @comment.save
+ 
     redirect_to article_path(@article)
   end
 
   def comment_params
-    params.require(:comment).permit(:author, :body)
+    params.require(:comment).permit(:body)
   end
 end
